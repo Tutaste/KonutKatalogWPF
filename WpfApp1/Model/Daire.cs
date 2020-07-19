ffeee9053aa6bfee913524567bb005536cbe4742
Aycan Özyılmaz <aycanozyilmaz@gmail.com>
2020-07-19 22:02:15 +0300

    Add project files.

---
+++ WpfApp1/Model/Daire.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public class Daire : Konut
+    {
+        public int Fiyat { get; set; }
+    }
+}
---
+++ WpfApp1/ViewModel/Liste.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    class Liste
+    {
+        private XmlSerializer serializer;
+        private FileStream file;
+        private List<Konut> konutlar = new List<Konut>();
+
+        public Liste()
+        {
+
+            konutlar.Add(new Daire() { Alan = 100, Fiyat = 500 });
+            serializer = new XmlSerializer(typeof(List<Konut>));
+            file = File.Open("Konutlar.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            ReadXML();
+        }
+
+        public List<Konut> Konutlar
+        {
+            get { return konutlar; }
+            set
+            {
+                konutlar = value;
+            }
+        }
+
+        public void ReadXML()
+        {
+            konutlar = (List<Konut>)serializer.Deserialize(file);
+        }
+
+        public void WriteXML()
+        {
+            serializer.Serialize(file, konutlar);
+        }
+    }
+}
